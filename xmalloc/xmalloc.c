2ea081ca5bc662f9e1788774df3a9c141de71c7f
Carlos Barajas <barajasc@umbc.edu>
2020-10-04 17:05:20 -0400

    Feedback to change to strict equality

--- xmalloc/xmalloc.c
+++ xmalloc/xmalloc.c
@@ -4,7 +4,7 @@
 
 void *malloc_or_exit(size_t nbytes, const char *file, int line) {
     void *x;
-    if ((nbytes <= 0) || ((x = malloc(nbytes)) == NULL)) {
+    if ((nbytes == 0) || ((x = malloc(nbytes)) == NULL)) {
         fprintf(stderr, "%s:line %d: malloc() of %zu bytes failed!\n",
                 file, line, nbytes);
         exit(EXIT_FAILURE);
