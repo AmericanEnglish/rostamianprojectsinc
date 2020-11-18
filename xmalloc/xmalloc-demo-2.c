6fdb5adb99e8999d084c80485c9ff940079251a5
Carlos Barajas <barajasc@umbc.edu>
2020-11-17 22:14:59 -0500

    fixed a small compiler warning from old

--- xmalloc/xmalloc-demo-2.c
+++ xmalloc/xmalloc-demo-2.c
@@ -4,10 +4,12 @@
 int main(int argc, char *argv[]) {
     int GB = 1e9;
     int iter = 0;
+    void *x;
     while (1) {
-        void *x = xmalloc(GB);
+        x = xmalloc(GB);
         iter++;
         printf("%3d allocating %d bytes\n", iter, GB);
     }
+    free(x);
     return 0;
 }
